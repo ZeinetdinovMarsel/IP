e63cadb35702704958ff732714b362df0a954efe
ZeinetdinovMarsel <98731035+ZeinetdinovMarsel@users.noreply.github.com>
2022-05-10 18:18:53 +0400

    Добавления
    Добавлена 3 лаба МЛИДМ

--- mldm.php
+++ mldm.php
@@ -12,19 +12,10 @@
         <div class="headText">
             Мои лабораторные работы МЛИДМ
         </div>
-        <?php
-        if($_COOKIE['user']==''):
-    ?>
-    <a href="userenter.php"><div class="user">
-        Вход/Регистрация
-    </div>
-    </a>
-    <?php else:?>
-        <div class="username">Пользователь:<a href="userpage.php" class="username"><?=$_COOKIE['user']?></a> <a href="/exit.php">Выход</a></div>
-    <?php endif;?>
     </div>
 <a href="mldmlab1.php"><div class=buttontext><br>Первая лабораторная работа</div></a><br>
 <a href="mldmlab2.php"><div class=buttontext><br>Вторая лабораторная работа</div></a>
+<a href="mldmlab3.php"><div class=buttontext><br>Третья лабораторная работа</div></a>
 </div>
 </body>
 
